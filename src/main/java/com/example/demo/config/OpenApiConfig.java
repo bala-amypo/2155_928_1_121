e464d9b23acfc6daf027c1a9bd4fda19b4bb70f8
bala-amypo <bala@amypo.in>
2025-12-23 06:20:27 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -0,0 +1,15 @@
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+            .components(new Components()
+                .addSecuritySchemes("bearerAuth",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
+    }
+}
