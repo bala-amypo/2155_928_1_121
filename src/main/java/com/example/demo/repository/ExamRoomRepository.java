2d02ab264a9383aa878515e72f6d24c5475681e1
bala-amypo <bala@amypo.in>
2025-12-25 14:04:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/ExamRoomRepository.java
+++ src/main/java/com/example/demo/repository/ExamRoomRepository.java
@@ -3,5 +3,9 @@ package com.example.demo.repository;
 import com.example.demo.model.ExamRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExamRoomRepository extends JpaRepository<ExamRoom, Long> {
+
+    List<ExamRoom> findByCapacityGreaterThanEqual(int capacity);
 }
