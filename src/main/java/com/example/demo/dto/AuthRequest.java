690c9029d88dee1809411c8807ab4f79c6155534
bala-amypo <bala@amypo.in>
2025-12-26 06:24:32 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -1,26 +1,41 @@
+// package com.example.demo.dto;
+
+// public class AuthRequest {
+
+//     private String email;
+//     private String password;
+
+//     public AuthRequest() {
+//     }
+
+//     public String getEmail() {
+//         return email;
+//     }
+
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
+
+//     public String getPassword() {
+//         return password;
+//     }
+
+//     public void setPassword(String password) {
+//         this.password = password;
+//     }
+// }
 package com.example.demo.dto;
 
-public class AuthRequest {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthRequest {
     private String email;
     private String password;
-
-    public AuthRequest() {
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,44 +1,59 @@
+// package com.example.demo.dto;
+
+// public class AuthResponse {
+
+//     private String token;
+//     private Long userId;
+//     private String email;
+//     private String role;
+
+//     public AuthResponse() {
+//     }
+
+//     public String getToken() {
+//         return token;
+//     }
+
+//     public void setToken(String token) {
+//         this.token = token;
+//     }
+
+//     public Long getUserId() {
+//         return userId;
+//     }
+
+//     public void setUserId(Long userId) {
+//         this.userId = userId;
+//     }
+
+//     public String getEmail() {
+//         return email;
+//     }
+
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
+
+//     public String getRole() {
+//         return role;
+//     }
+
+//     public void setRole(String role) {
+//         this.role = role;
+//     }
+// }
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
-
-    private String token;
-    private Long userId;
-    private String email;
-    private String role;
-
-    public AuthResponse() {
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
+    private String accessToken;
+    private String tokenType; // usually "Bearer"
 }
