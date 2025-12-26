b948cb8adf0a4032bbad5c4195b5cd6b57f5d0e1
bala-amypo <bala@amypo.in>
2025-12-26 05:36:33 +0000

    User commit

--- src/main/java/com/example/demo/exception/RestExceptionHandler.java
+++ src/main/java/com/example/demo/exception/RestExceptionHandler.java
@@ -1,3 +1,18 @@
+// package com.example.demo.exception;
+
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.ExceptionHandler;
+// import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// @RestControllerAdvice
+// public class RestExceptionHandler {
+
+//     @ExceptionHandler(ApiException.class)
+//     public ResponseEntity<String> handleApi(ApiException ex) {
+//         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+//     }
+// }
 package com.example.demo.exception;
 
 import org.springframework.http.HttpStatus;
@@ -7,9 +22,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
-
     @ExceptionHandler(ApiException.class)
-    public ResponseEntity<String> handleApi(ApiException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> handleApiException(ApiException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST); // Using 400 for logic errors
     }
 }
