e64c9bc131499e6dfdf62160f7c5a089f04025e2
bala-amypo <bala@amypo.in>
2025-12-28 06:00:18 +0000

    User commit

--- src/main/java/com/example/demo/repository/ExamRoomRepository.java
+++ src/main/java/com/example/demo/repository/ExamRoomRepository.java
@@ -1,14 +1,4 @@
-// package com.example.demo.repository;
 
-// import com.example.demo.model.ExamRoom;
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// import java.util.List;
-
-// public interface ExamRoomRepository extends JpaRepository<ExamRoom, Long> {
-
-//     List<ExamRoom> findByCapacityGreaterThanEqual(int capacity);
-// }
 package com.example.demo.repository;
 import com.example.demo.model.ExamRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,39 +1,4 @@
-// package com.example.demo.security;
 
-// import io.jsonwebtoken.*;
-// import io.jsonwebtoken.security.Keys;
-// import org.springframework.stereotype.Component;
-
-// import java.security.Key;
-// import java.util.Date;
-
-// @Component
-// public class JwtTokenProvider {
-
-//     private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-//     private final long EXPIRATION = 1000 * 60 * 60; // 1 hour
-
-//     public String generateToken(Long userId, String email, String role) {
-
-//         return Jwts.builder()
-//                 .setSubject(email)
-//                 .claim("userId", userId)
-//                 .claim("role", role)
-//                 .setIssuedAt(new Date())
-//                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
-//                 .signWith(key)
-//                 .compact();
-//     }
-
-//     public String getEmail(String token) {
-//         return Jwts.parserBuilder()
-//                 .setSigningKey(key)
-//                 .build()
-//                 .parseClaimsJws(token)
-//                 .getBody()
-//                 .getSubject();
-//     }
-// }
 package com.example.demo.security;
 
 import io.jsonwebtoken.*;
